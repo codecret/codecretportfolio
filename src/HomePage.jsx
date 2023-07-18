ced6d310390c56e4f4f640b326f65a27b382cde5
Y7gn <malekkhodary@hotmail.com>
2023-07-18 07:42:11 +0300

    first-commit

---
+++ src/HomePage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  Home,
+} from "./components";
+
+const HomePage = () => {
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className=" bg-cover bg-no-repeat bg-center">
+        <Home />
+      </div>
+      <About />
+      {/* <Experience /> */}
+      {/* <Tech /> */}
+      <Works />
+      <Hero />
+      {/* <Feedbacks /> */}
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/SharedLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from "react-router-dom";
+import { Navbar, Footer } from "./components";
+
+const SharedLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default SharedLayout;
