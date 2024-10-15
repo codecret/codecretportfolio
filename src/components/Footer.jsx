293704f8220e7392124f3743149683e37372d3b6
codecret <mohammedmalek2016@gmail.com>
2024-10-15 16:31:42 +0300

    change instagram

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
             <AiFillInstagram
               className="icon-social cursor-pointer"
               onClick={() =>
-                window.open("https://www.instagram.com/codecret2/", "_blank")
+                window.open("https://www.instagram.com/codecret1/", "_blank")
               }
             />
             <AiFillGithub
